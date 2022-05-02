535cc38a21a82a9213051c22cd40e3b69e770961
Nate-Huerd <nhuerd3@gmail.com>
2022-05-02 15:09:58 -0500

    adjusted connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,14 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 require('dotenv').config();
 
-// create connection to our db
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-  host: 'localhost',
-  dialect: 'mysql',
-  port: 3306
+const connection = mysql.createConnection({
+  host: "localhost",
+  // Your username
+  user: "root",
+  // Your password
+  password: "",
+  database: "employees"
 });
 
-module.exports = sequelize;
+module.exports = db;
